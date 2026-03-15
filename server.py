f3007048345279fcd4dae7a75f240b539e714ce4
chotaboy <piccafacts@gmail.com>
2026-03-15 13:43:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -222,5 +223,6 @@ def status():
     return jsonify({"status":"online"})
 
 # ---------------- RUN SERVER ---------------- #
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
